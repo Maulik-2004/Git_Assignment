4d5e4bb82767a2d99a5433e18c7c6dba7311cc69
Maulik-2004 <maulikdarbar2004@gmail.com>
2026-03-19 11:05:43 +0530

    JavaScript Assignment

---
+++ Assignment5.js
@@ -0,0 +1,10 @@
+//Assignment 5: Shopping Eligibility Checker
+
+
+const isMember = true;
+const cartvalue = 5000;
+if (isMember && cartvalue > 1000) {
+    console.log("You are eligible for free delivery!");
+} else {    
+    console.log("Delivery charges will be applied!");
+}
